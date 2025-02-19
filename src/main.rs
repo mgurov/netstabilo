16cdc23e53428a07fde5b2f41396d3fde0ea7de4
Mykola Gurov <ngurov@gmail.com>
2025-02-19 11:49:35 +0100

    ping mode for using the shell ping

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use std::thread;
 use std::time::Duration as dur;
-use std::net::TcpStream;
+use std::process::Command;
 use std::io::{self, Write};
 use chrono::{Local, Duration};
+use std::net::TcpStream;
+use std::env;
 
-fn check_network_connection() -> bool {
-    // Try to connect to a reliable server, e.g., Google's public DNS
-    if let Ok(_) = TcpStream::connect("8.8.8.8:53") {
-        return true;
+type NetworkCheckFn = fn() -> bool;
+
+fn check_network_connection_ping() -> bool {
+    // Execute the ping command
+    let output = Command::new("ping")
+        .arg("-c")
+        .arg("1")
+        .arg("8.8.8.8")
+        .output();
+
+    match output {
+        Ok(output) => output.status.success(),
+        Err(_) => false,
     }
-    false
 }
+
+fn check_network_connection_tcp() -> bool {
+        if let Ok(_) = TcpStream::connect("8.8.8.8:53") {
+            return true;
+        }
+        false
+ }
 
 fn now() -> String {
     Local::now().format("%H:%M:%S").to_string()
@@ -29,10 +46,28 @@ const OK: &str = "\x1b[32mOK\x1b[0m";
 const FAIL: &str = "\x1b[31mFAIL\x1b[0m";
 
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    // Define the running mode based on the first argument
+    let running_mode = if args.len() > 1 {
+        &args[1]
+    } else {
+        "default"
+    };
+
+    let network_check: NetworkCheckFn = if running_mode == "ping" {
+        println!("using ping");
+        check_network_connection_ping
+    } else {
+        println!("using tcp::connect");
+        check_network_connection_tcp
+    };
+
     let mut seconds_elapsed: i64 = 0;
     let mut last_status = "initial";
     loop {
-        let current_status = if check_network_connection() {OK} else {FAIL};
+        let current_status = if network_check() {OK} else {FAIL};
         if current_status != last_status {
             println!("\n{} {}", now(), current_status);
             last_status = current_status;
